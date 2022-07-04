a1951a1ed7e0b495c1075a230a4a938b9f248b15
Renan Dantas <renandantas.sp@gmail.com>
2022-07-03 23:59:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const text = "Auto write text by RDev22!";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
